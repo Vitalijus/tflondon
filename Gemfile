c7386e78510039bf989f089eb4264782b198eede
Vitalijus Desukas <vitalij.desuk@gmail.com>
2016-12-01 14:02:12 +0000

    add httparty gem and get GET request with parsed json object

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'gmaps4rails'
-gem 'underscore-rails' 
+gem 'underscore-rails'
+gem 'httparty'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -1,2 +1,8 @@
 class Traffic < ApplicationRecord
+
+  response = HTTParty.get("https://api.tfl.gov.uk/Road/all/Street/Disruption?startDate=2016-12-01&endDate=2016-12-31")
+  
+  # JSON.parse parses response.body into a ruby object.
+  JSON.parse(response.body)
+
 end
