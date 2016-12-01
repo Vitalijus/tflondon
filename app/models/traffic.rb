4e7759860bbb2a48b20048e01ca9cac94debdd66
Vitalijus Desukas <vitalij.desuk@gmail.com>
2016-12-01 14:11:24 +0000

    add get_request method and jsonpath gem

--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -4,9 +4,11 @@ class Traffic < ApplicationRecord
   base_uri "https://api.tfl.gov.uk/Road/all/Street/Disruption?"
   format :json
 
-  response = HTTParty.get("startDate=2016-12-01&endDate=2016-12-31")
+  def get_request
+  	response = self.class.get("startDate=2016-12-01&endDate=2016-12-31")
   
-  # JSON.parse parses response.body into a ruby object.
-  JSON.parse(response.body)
+  	# JSON.parse parses response.body into a ruby object.
+  	JSON.parse(response.body)
+	end
 
 end
