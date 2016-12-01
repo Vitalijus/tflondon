98cbb4e75e3ff4d5594fd229c59801ea99cf4533
Vitalijus Desukas <vitalij.desuk@gmail.com>
2016-12-01 14:05:06 +0000

    add httparty base_uri and formas as json

--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -1,6 +1,10 @@
 class Traffic < ApplicationRecord
 
-  response = HTTParty.get("https://api.tfl.gov.uk/Road/all/Street/Disruption?startDate=2016-12-01&endDate=2016-12-31")
+	include HTTParty
+  base_uri "https://api.tfl.gov.uk/Road/all/Street/Disruption?"
+  format :json
+
+  response = HTTParty.get("startDate=2016-12-01&endDate=2016-12-31")
   
   # JSON.parse parses response.body into a ruby object.
   JSON.parse(response.body)
